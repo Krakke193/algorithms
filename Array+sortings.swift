76a6b6c7f97103e4716a80c17ea0d6b32c4619a4
Andrew Olkhovsky <andrew.olkhovsky@gmail.com>
2019-04-06 14:15:11 +0300

    refacoted code structure to cli make compilation easier

--- Array+sortings.swift
+++ Array+sortings.swift
@@ -78,13 +78,3 @@ extension Array where Element: Comparable {
         print(self)
     }
 }
-
-
-
-var small = [5,6,2,9,7]
-var big = (1...42).shuffled()
-var text = (97...122)
-    .map({Character(UnicodeScalar($0))})
-    .shuffled()
-
-big.mergeSort()
--- fibonacci.swift
+++ fibonacci.swift
@@ -1,3 +1,5 @@
+// TODO: Add dynamic programming example
+
 func fibonacci(_ n: Int) -> Int {
     if n < 2 {
         return 1
@@ -5,9 +7,3 @@ func fibonacci(_ n: Int) -> Int {
         return fibonacci(n - 2) + fibonacci(n - 1)
     }
 }
-
-// TODO: Add dynamic programming example
-
-let n = 5
-let res = fibonacci(n)
-print("\(n)th fibonacci number is: \(res)")
