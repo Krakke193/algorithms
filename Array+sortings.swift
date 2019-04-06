6a7cd8fdc98d619dcd13272df3c75453410aa73b
Andrew Olkhovsky <andrew.olkhovsky@gmail.com>
2019-04-06 12:01:43 +0300

    added bubble sort

--- Array+sortings.swift
+++ Array+sortings.swift
@@ -1,4 +1,16 @@
 extension Array where Element: Comparable {
+	mutating func bubbleSort() {
+		for i in 0..<self.count {
+			for j in 1..<self.count - i {
+				if self[j - 1] > self[j] {
+					self.swapAt(j - 1, j)
+				}
+			}
+		}
+
+		print(self)
+	}
+
 	mutating func insertionSort() {
 		for var i in 1..<self.count {
 			let temp = self[i]
@@ -8,6 +20,8 @@ extension Array where Element: Comparable {
 			}
 			self[i] = temp
 		}
+
+		print(self)
 	}
 }
 
@@ -17,5 +31,4 @@ var small = [5,6,2,9]
 var big = [5,6,2,9,5,7,1,0,2,4,9]
 var text = ["z", "a", "x", "c", "t"]
 
-text.insertionSort()
-print(text)
+big.bubbleSort()
