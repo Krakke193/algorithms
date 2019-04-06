ccd190d28235429a815377c17b05137e37dcd125
Andrew Olkhovsky <andrew.olkhovsky@gmail.com>
2019-04-06 12:54:42 +0300

    added merge sort

--- Array+sortings.swift
+++ Array+sortings.swift
@@ -23,12 +23,68 @@ extension Array where Element: Comparable {
 
 		print(self)
 	}
+
+	mutating func mergeSort() {
+
+		func sort(_ arr: Array) -> Array {
+			guard arr.count > 1 else { return arr }
+
+			let middle = arr.count / 2
+			let left = sort(Array(arr[0..<middle]))
+			let right = sort(Array(arr[middle..<arr.count]))
+
+			return merge(l: left, r: right)
+		}
+
+		func merge(l left: Array, r right: Array) -> Array {
+			var leftIndex = 0
+			var rightIndex = 0
+
+			var merged = [Element]()
+			merged.reserveCapacity(left.count + right.count)
+
+			while leftIndex < left.count && rightIndex < right.count {
+				if left[leftIndex] < right[rightIndex] {
+					merged.append(left[leftIndex])
+					leftIndex += 1
+				} else if left[leftIndex] > right[rightIndex] {
+					merged.append(right[rightIndex])
+					rightIndex += 1
+				} else {
+					// elements are equal – add both and increase indexes
+					merged.append(left[leftIndex])
+					merged.append(right[rightIndex])
+
+					leftIndex += 1
+					rightIndex += 1
+				}
+			}
+
+			// some elements may be left in either left or right array
+			while leftIndex < left.count {
+				merged.append(left[leftIndex])
+				leftIndex += 1
+			}
+
+			while rightIndex < right.count {
+				merged.append(right[rightIndex])
+				rightIndex += 1
+			}
+
+			return merged
+		}
+
+		self = sort(self)
+		print(self)
+	}
 }
 
 
 
-var small = [5,6,2,9]
-var big = [5,6,2,9,5,7,1,0,2,4,9]
-var text = ["z", "a", "x", "c", "t"]
+var small = [5,6,2,9,7]
+var big = (1...42).shuffled()
+var text = (97...122)
+	.map({Character(UnicodeScalar($0))})
+	.shuffled()
 
-big.bubbleSort()
+big.mergeSort()
